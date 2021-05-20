b199d1b1666e95c4e7075ab303ffe80d9d72d04c
Shane King <shane.king.2008@gmail.com>
2021-05-19 21:35:20 -0400

    Adding support for reveal, adding some frames!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+// @ts-ignore
+import Reveal from 'reveal.js'
+// @ts-ignore
+import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+// @ts-ignore
+import Highlight from 'reveal.js/plugin/highlight/highlight.esm.js';
+// @ts-ignore
+import Notes from 'reveal.js/plugin/notes/notes.esm.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'supabase-reveal';
+export class AppComponent implements AfterViewInit {
+  public code: string = `class Test {
+  public static a: string = 'a';
+
+  public run(): void {
+    console.log('Hello world!);
+  }
+}`;
+
+  public ngAfterViewInit(): void {
+    let deck = new Reveal({
+      plugins: [ Markdown, Highlight, Notes ]
+    })
+    deck.initialize();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TitleScreenComponent } from './sections/title-screen/title-screen.component';
+import { RemoveHostDirective } from './directives/remove-host.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleScreenComponent,
+    RemoveHostDirective
   ],
   imports: [
     BrowserModule
